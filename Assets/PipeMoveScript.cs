1d98cb53ac6d63ff1449db1ac0ed34448bd9338f
jerquinigo <jonathanerquinigo@pursuit.org>
2023-01-18 21:17:34 -0500

    adding project

---
+++ Assets/PipeMoveScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float moveSpeed = 5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // vector three is needed because of the x,y,z space. Unity is still a 3d engine
+        // Time.deltaTime will happen at its designated time, no matter the frame rate.
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+    }
+}
