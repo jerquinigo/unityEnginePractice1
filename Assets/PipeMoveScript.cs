b5fc2bb87c93504929b1b11ab6b489fc9e9b9600
jerquinigo <jonathanerquinigo@pursuit.org>
2023-01-18 21:23:44 -0500

    updating notes

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -7,6 +7,7 @@ public class PipeMoveScript : MonoBehaviour
     // Start is called before the first frame update
 
     public float moveSpeed = 5;
+    public float deadZone = -45;
     void Start()
     {
         
@@ -18,5 +19,10 @@ public class PipeMoveScript : MonoBehaviour
         // vector three is needed because of the x,y,z space. Unity is still a 3d engine
         // Time.deltaTime will happen at its designated time, no matter the frame rate.
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+
+        if (transform.position.x < deadZone) {
+            Debug.Log("pipe deleted");
+            Destroy(gameObject);
+        }
     }
 }
